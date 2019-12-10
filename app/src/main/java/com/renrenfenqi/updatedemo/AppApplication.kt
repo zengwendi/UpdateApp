d88751e227f6842212218980e74c1c30cebeb5d2
goat <zengwendi@jinzaofintech.com>
2019-12-10 12:07:52 +0800

    解决冲突

---
+++ app/src/main/java/com/renrenfenqi/updatedemo/AppApplication.kt
@@ -0,0 +1,10 @@
+package com.renrenfenqi.updatedemo
+
+import android.app.Application
+
+/**
+ * @Author dean.zeng
+ * @Description TODO
+ * @Date 2019-12-10 09:58
+ **/
+class AppApplication:Application(){}
